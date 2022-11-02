5138c6c78b24a9c65f9ee26e076cbab3128fd1ba
Mogoww <gauthier.mauche@gmail.com>
2022-11-02 14:30:52 +0100

    create etudiant class

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,2 +1,38 @@
 public class Etudiant {
+
+    // Attributs
+    private int numEtudiant;
+    private String nom;
+    private String prenom;
+
+
+    // getters et setters
+
+    public void setNumEtudiant(int numEtudiant) {
+        this.numEtudiant = numEtudiant;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    // Constructeur
+    public Etudiant(int numEtudiant, String nom, String prenom) {
+        this.numEtudiant = numEtudiant;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
 }
