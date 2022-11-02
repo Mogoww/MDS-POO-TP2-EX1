8216444c3a8c31449639150324126b9e7e73a630
Mogoww <gauthier.mauche@gmail.com>
2022-11-02 15:00:37 +0100

    add SuiviPromo

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        GroupeTD test = new GroupeTD();
-
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/SuiviPromo.java
@@ -0,0 +1,7 @@
+public class SuiviPromo {
+    public static void main(String[] args) {
+        GroupeTD groupeTD = new GroupeTD();
+        groupeTD.ajouterEtudiant(1, "Dupont", "Jean");
+        groupeTD.afficherEtudiants();
+    }
+}
